8cbaed0372aa06aa469ac942288f8972a19d1835
Jorge <jorgesaiz06@gmail.com>
2025-12-27 18:46:34 +0100

    Pessimistic lock

--- account-service/src/main/java/com/bank/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/bank/accountservice/repository/AccountRepository.java
@@ -1,10 +1,12 @@
 package com.bank.accountservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import com.bank.accountservice.model.account.Account;
 
+import jakarta.persistence.LockModeType;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,6 +14,7 @@ import java.util.UUID;
 public interface AccountRepository extends JpaRepository<Account, UUID> {
     Optional<Account> findByAccountNumber(String accountNumber);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT a FROM Account a WHERE a.id = :fromAccountId")
     Optional<Account> findByIdWithLock(UUID fromAccountId);
 }

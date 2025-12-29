4fbcee546d076f205ab23e930b4c78c3657afd09
Jorge <jorgesaiz06@gmail.com>
2025-12-29 13:20:48 +0100

    Index

---
+++ account-service/src/main/resources/db/migration/V5__index.sql
@@ -0,0 +1,5 @@
+CREATE UNIQUE INDEX IF NOT EXISTS ux_account_account_number
+ON accounts (account_number);
+
+CREATE INDEX IF NOT EXISTS idx_outbox_event_status_created_at 
+ON outbox_event (status, created_at);
